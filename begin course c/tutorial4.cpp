cf8a417126ab0b27286a614b74e3d8362b825abf
vikash_seth.oops <sethv4506@gmail.com>
2024-06-30 08:37:21 +0530

    my c++ learning repo

---
+++ begin course c/tutorial4.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a = 24;
+    cout<<"hello world"<<a<<endl;
+    int b = 24,c = 34;
+    cout<<"the value of a is"<<a<<"\nthe value of c is"<<c<<endl;
+    // this is our program of datatypes
+    float pi = 3.14;
+    
+    cout<<"the value of pi is: "<<pi<<endl<<"my brother name is tondu bhai"<<pi; 
+    double bholu = 287.987;
+    cout<<"\nbholu was a two piece: "<<bholu<<endl<<"charming place: "<<a;
+    return 0;
+}
