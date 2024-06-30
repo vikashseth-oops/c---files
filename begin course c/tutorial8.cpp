a4652b33f831d5d44d6abcaeea396fbde2f5cc98
vikash_seth.oops <sethv4506@gmail.com>
2024-06-30 08:50:42 +0530

    my c++ learning repo

--- begin course c/tutorial8.cpp
+++ begin course c/tutorial8.cpp
@@ -1,3 +1,25 @@
 #include<iostream>
 using namespace std;
 
+int main()
+{
+    int a = 0;
+    // cout<<a<<endl;
+    // a++;
+    //  cout<<a<<endl;
+    // a++;
+    //  cout<<a<<endl;
+    // a++;
+    //  cout<<a<<endl;
+    // a++;
+    //  cout<<a<<endl;
+    // a++;
+    //  cout<<a<<endl;
+    // a++;
+    for (int a = 0; a <= 40; a++)
+    {
+        cout<<a<<endl;
+    }
+    
+    return 0;
+}
